e33fc839eddf938d67bf25b3081d7ffe22f88ff3
butterfly-react <rdigital4455@gmail.com>
2024-06-01 09:09:57 +0200

    multistep-form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
 import Multistep from '@/components/MultiStep';
+import EditableUserList from '@/components/EditableUserList';
 
 type Props = {};
 
 function Page({}: Props) {
   return (
     <Box width="100%" height="100vh" bg="gray.100">
-      <Multistep />
+      <EditableUserList />
     </Box>
   );
 }
---
+++ src/components/EditableUserList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function EditableUserList({}: Props) {
+  return (
+    <div>U</div>
+  )
+}
+
+export default EditableUserList
