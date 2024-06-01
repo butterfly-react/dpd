875be3afd1ab4edb5e63745b53378dbbb53aec1f
butterfly-react <rdigital4455@gmail.com>
2024-06-01 16:50:58 +0200

    dashboard

--- src/components/MultiStep.tsx
+++ src/components/MultiStep.tsx
@@ -517,7 +517,7 @@ export default function Multistep() {
             bg="rgba(128, 128, 128, 0.2)"
             _hover={{ bg: "rgba(128, 128, 128, 0.7)" }}
           >
-            <Link href="/dashboard">{`>>>`}</Link>
+            <Link href="/dashboard">Dashboard</Link>
           </Button>
         </Flex>
         <Progress
